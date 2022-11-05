e03e2c93a6171a3c9ac79a13c74b6d5fc5e57253
Lilian VERLHAC <lilian.verlhac@outlook.com>
2022-11-05 20:48:04 +0100

    Add lexer for {}()!:,

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,7 @@
+use std::str::Chars;
+
 #[derive(Debug)]
+#[cfg_attr(test, derive(PartialEq, Eq))]
 pub enum Token {
     Word(String),
     NamespaceSeparator,
@@ -10,56 +13,73 @@ pub enum Token {
     CloseArguments,
 }
 
-// ugly shit omg use recursivity idiot !!!!
 pub fn lexer(str: &str) -> Vec<Token> {
-    let (tokens, _) =
-        str.chars()
-            .fold((vec![], Some(String::new())), |(mut tokens, word), char| {
-                match (char, word) {
-                    (',', None) => tokens.push(Token::LineSeparator),
-                    (',', Some(word)) => {
-                        tokens.push(Token::Word(word));
-                        tokens.push(Token::LineSeparator);
-                    }
-                    ('(', None) => tokens.push(Token::OpenArguments),
-                    ('(', Some(word)) => {
-                        tokens.push(Token::Word(word));
-                        tokens.push(Token::OpenArguments);
-                    }
-                    (')', None) => tokens.push(Token::CloseArguments),
-                    (')', Some(word)) => {
-                        tokens.push(Token::Word(word));
-                        tokens.push(Token::CloseArguments);
-                    }
-                    ('{', None) => tokens.push(Token::OpenObject),
-                    ('{', Some(word)) => {
-                        tokens.push(Token::Word(word));
-                        tokens.push(Token::OpenObject);
-                    }
-                    ('}', None) => tokens.push(Token::CloseObject),
-                    ('}', Some(word)) => {
-                        tokens.push(Token::Word(word));
-                        tokens.push(Token::CloseObject);
-                    }
-                    (':', None) => tokens.push(Token::NameSeparator),
-                    (':', Some(word)) => {
-                        tokens.push(Token::Word(word));
-                        tokens.push(Token::NameSeparator);
-                    }
-                    ('!', None) => tokens.push(Token::NamespaceSeparator),
-                    ('!', Some(word)) => {
-                        tokens.push(Token::Word(word));
-                        tokens.push(Token::NamespaceSeparator);
-                    }
-                    (' ', word) => return (tokens, word),
-                    ('\n', word) => return (tokens, word),
-                    (c, None) => return (tokens, Some(String::from(c))),
-                    (c, Some(mut word)) => {
-                        word.push(c);
-                        return (tokens, Some(word.to_string()));
-                    }
-                };
-                (tokens, None)
-            });
+    let mut tokens = Vec::new();
+
+    lexerino(&mut str.chars(), &mut tokens);
     tokens
+}
+
+fn lexerino(char_iterator: &mut Chars, tokens: &mut Vec<Token>) {
+    if let Some(char) = char_iterator.next() {
+        match char {
+            ',' => tokens.push(Token::LineSeparator),
+            '(' => tokens.push(Token::OpenArguments),
+            ')' => tokens.push(Token::CloseArguments),
+            '{' => tokens.push(Token::OpenObject),
+            '}' => tokens.push(Token::CloseObject),
+            ':' => tokens.push(Token::NameSeparator),
+            '!' => tokens.push(Token::NamespaceSeparator),
+            c if c.is_whitespace() => (),
+            _ => unimplemented!(),
+        };
+        lexerino(char_iterator, tokens);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn lexer_with_empty_input_should_return_empty_vector() {
+        assert_eq!(0, lexer("").len())
+    }
+
+    #[test]
+    fn lexer_with_all_the_syntax_element_as_input_should_return_a_vector_with_all_those_elements() {
+        let expected_vector = Vec::from([
+            Token::OpenObject,
+            Token::CloseObject,
+            Token::OpenArguments,
+            Token::CloseArguments,
+            Token::NamespaceSeparator,
+            Token::LineSeparator,
+            Token::NameSeparator,
+        ]);
+
+        assert_eq!(expected_vector, lexer("{}()!,:"))
+    }
+
+    #[test]
+    fn lexer_should_ignore_whitespaces() {
+        let expected_vector = Vec::from([
+            Token::OpenObject,
+            Token::CloseObject,
+            Token::OpenArguments,
+            Token::CloseArguments,
+            Token::NamespaceSeparator,
+            Token::LineSeparator,
+            Token::NameSeparator,
+        ]);
+
+        assert_eq!(expected_vector, lexer("{}   ()    !  \n\t ,:\r"))
+    }
+
+    #[test]
+    fn lexer_should_retrieve_unsyntaxic_characters_as_a_word() {
+        let expected_vector = Vec::from([Token::Word(String::from("Test"))]);
+
+        assert_eq!(expected_vector, lexer("Test"))
+    }
 }
