a1e53ce534bc9ea84fe1604f8b6c1053d11387af
Lilian VERLHAC <lilian.verlhac@outlook.com>
2022-11-06 22:21:54 +0100

    begin parser for object

--- src/parser.rs
+++ src/parser.rs
@@ -2,24 +2,46 @@ use crate::lexer::Token;
 
 #[derive(Debug)]
 #[cfg_attr(test, derive(PartialEq))]
-enum Number {
-    Integer(i64),
-    Decimal(f64),
+pub enum AST {
+    Numeric(Number),
+    String(String),
+    Array(Vec<AST>),
 }
 
 #[derive(Debug)]
 #[cfg_attr(test, derive(PartialEq))]
-pub enum AST {
-    Numeric(Number),
+pub enum Number {
+    Integer(i64),
+    Decimal(f64),
 }
 
 pub fn parser(tokens: Vec<Token>) -> Option<AST> {
     match tokens.as_slice() {
         [] => None,
+        [Token::OpenObject, tail @ ..] => Some(AST::Array(parse_array_or_object(tail)?)),
+        _ => None,
+    }
+}
+
+fn parse_array_or_object(tokens: &[Token]) -> Option<Vec<AST>> {
+    match tokens {
+        [_token, Token::LineSeparator, _tail @ ..] => todo!(),
+        [_token, Token::CloseObject, _tail @ ..] => todo!(),
+        [Token::CloseObject, _tail @ ..] => todo!(),
+        _ => todo!(),
+    }
+}
+
+fn parse(tokens: &[Token]) -> Option<AST> {
+    match tokens {
+        [] => None,
         [Token::Word(word)] if word.chars().find(|&c| c == '.').is_none() => {
             Some(AST::Numeric(Number::Integer(word.parse().ok()?)))
         }
         [Token::Word(word)] => Some(AST::Numeric(Number::Decimal(word.parse().ok()?))),
+        [Token::StringLiteralSeparator, Token::Word(word), Token::StringLiteralSeparator] => {
+            Some(AST::String(word.to_owned()))
+        }
         _ => None,
     }
 }
@@ -37,6 +59,7 @@ mod tests {
             Some(AST::Numeric(expected_number))
         )
     }
+
     #[test]
     fn should_parse_word_with_decimal_as_number_decimal() {
         let expected_number = Number::Decimal(12.24);
@@ -44,6 +67,30 @@ mod tests {
         assert_eq!(
             parser(vec![Token::Word("12.24".to_string())]),
             Some(AST::Numeric(expected_number))
+        )
+    }
+
+    #[test]
+    fn should_parse_string_litteral_as_string() {
+        let expected_ast = AST::String("Toto".to_string());
+
+        assert_eq!(
+            parser(vec![
+                Token::StringLiteralSeparator,
+                Token::Word("Toto".to_string()),
+                Token::StringLiteralSeparator
+            ]),
+            Some(expected_ast)
+        )
+    }
+
+    #[test]
+    fn should_parse_list_objects_as_lists() {
+        let expected_ast = AST::Array(vec![]);
+
+        assert_eq!(
+            parser(vec![Token::OpenObject, Token::CloseObject]),
+            Some(expected_ast)
         )
     }
 }
